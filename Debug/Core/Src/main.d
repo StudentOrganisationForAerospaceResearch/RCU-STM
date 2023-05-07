a66aeaa869607f67a3db38a66794256e462aacde
Marcelo Li Gonzales <marcelo.ligonzales@ucalgary.ca>
2023-05-06 20:16:21 -0600

    basic code generator setup no tasks no nothing

--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -1,4 +1,4 @@
-Core/Src/main.o: ../Core/Src/main.cpp ../Core/Inc/main.h \
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
  ../Core/Inc/stm32l4xx_hal_conf.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
@@ -52,9 +52,7 @@ Core/Src/main.o: ../Core/Src/main.cpp ../Core/Inc/main.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h \
- ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
- C:/Users/cjcha/OneDrive/Desktop/Git/RCU-STM/Components/main_avionics.hpp \
- C:/Users/cjcha/OneDrive/Desktop/Git/RCU-STM/Components/Core/Inc/Mutex.hpp
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h
 ../Core/Inc/main.h:
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
 ../Core/Inc/stm32l4xx_hal_conf.h:
@@ -110,5 +108,3 @@ Core/Src/main.o: ../Core/Src/main.cpp ../Core/Inc/main.h \
 ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
 ../Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h:
 ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
-C:/Users/cjcha/OneDrive/Desktop/Git/RCU-STM/Components/main_avionics.hpp:
-C:/Users/cjcha/OneDrive/Desktop/Git/RCU-STM/Components/Core/Inc/Mutex.hpp:
