c71c6cb3d88e19d7529007ecbb22944dd1d75fe1
shivamdesai04 <101665374+shivamdesai04@users.noreply.github.com>
2023-05-27 15:18:19 -0600

    enabled debug global inturrupt

--- Core/Src/stm32l4xx_hal_msp.c
+++ Core/Src/stm32l4xx_hal_msp.c
@@ -437,7 +437,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     PC11     ------> USART3_RX
     PD2     ------> USART3_DE
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
+    GPIO_InitStruct.Pin = RADIO_TX_Pin|RADIO_RX_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
@@ -451,6 +451,9 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
     HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
+    /* USART3 interrupt Init */
+    HAL_NVIC_SetPriority(USART3_IRQn, 5, 0);
+    HAL_NVIC_EnableIRQ(USART3_IRQn);
   /* USER CODE BEGIN USART3_MspInit 1 */
 
   /* USER CODE END USART3_MspInit 1 */
@@ -533,10 +536,12 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
     PC11     ------> USART3_RX
     PD2     ------> USART3_DE
     */
-    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_10|GPIO_PIN_11);
+    HAL_GPIO_DeInit(GPIOC, RADIO_TX_Pin|RADIO_RX_Pin);
 
     HAL_GPIO_DeInit(GPIOD, GPIO_PIN_2);
 
+    /* USART3 interrupt DeInit */
+    HAL_NVIC_DisableIRQ(USART3_IRQn);
   /* USER CODE BEGIN USART3_MspDeInit 1 */
 
   /* USER CODE END USART3_MspDeInit 1 */
