b3a8d85e797b5dd1935946ef1569fdcfc85dbe43
Jesse Gerbrandt <jgerbrandt@live.com>
2023-07-08 19:14:01 -0600

    was working now hardfaulting lmaoooo

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -256,7 +256,7 @@ static void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
-  sConfig.Channel = ADC_CHANNEL_3;
+  sConfig.Channel = ADC_CHANNEL_1;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_247CYCLES_5;
   sConfig.SingleDiff = ADC_SINGLE_ENDED;
@@ -452,7 +452,6 @@ static void MX_USART3_UART_Init(void)
   /* USER CODE END USART3_Init 1 */
   huart3.Instance = USART3;
   huart3.Init.BaudRate = 115200;
-  huart3.Init.BaudRate = 115200;
   huart3.Init.WordLength = UART_WORDLENGTH_8B;
   huart3.Init.StopBits = UART_STOPBITS_1;
   huart3.Init.Parity = UART_PARITY_NONE;
@@ -587,12 +586,12 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, CONT_CK0_Pin|CONT_CK1_Pin|TC1_NCS_Pin|TC2_NCS_Pin|TC1_NCS_Pin|TC2_NCS_Pin
+  HAL_GPIO_WritePin(GPIOC, CONT_CK0_Pin|CONT_CK1_Pin|TC1_NCS_Pin|TC2_NCS_Pin
                           |RS422_TX_EN_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, RELAY11_Pin|RELAY10_Pin|RELAY2_Pin|RELAY1_Pin
-                          |RELAY0_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOA, RELAY11_Pin|RELAY10_Pin|NOS2_LC_CLK_Pin|RELAY2_Pin
+                          |RELAY1_Pin|RELAY0_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, RELAY9_Pin|RELAY4_Pin|RELAY3_Pin, GPIO_PIN_SET);
@@ -601,17 +600,16 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOB, RELAY8_Pin|RELAY7_Pin|RELAY5_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, RELAY6_Pin|LED_2_Pin|LED_1_Pin|LED_0_Pin|NOS1_LC_CLK_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, RELAY6_Pin|LED_2_Pin|LED_1_Pin|LED_0_Pin
+                          |NOS1_LC_CLK_Pin, GPIO_PIN_RESET);
 
-    /*Configure GPIO pin Output Level */
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(USART1_TX_EN_GPIO_Port, USART1_TX_EN_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : CONT_CK0_Pin CONT_CK1_Pin NOS2_LC_CLK_Pin RELAY9_Pin
-                           TC1_NCS_Pin TC2_NCS_Pin RELAY4_Pin RELAY3_Pin
-                           RS422_TX_EN_Pin */
-  GPIO_InitStruct.Pin = CONT_CK0_Pin|CONT_CK1_Pin|NOS2_LC_CLK_Pin|RELAY9_Pin
-                          |TC1_NCS_Pin|TC2_NCS_Pin|RELAY4_Pin|RELAY3_Pin
-                          |RS422_TX_EN_Pin;
+  /*Configure GPIO pins : CONT_CK0_Pin CONT_CK1_Pin RELAY9_Pin TC1_NCS_Pin
+                           TC2_NCS_Pin RELAY4_Pin RELAY3_Pin RS422_TX_EN_Pin */
+  GPIO_InitStruct.Pin = CONT_CK0_Pin|CONT_CK1_Pin|RELAY9_Pin|TC1_NCS_Pin
+                          |TC2_NCS_Pin|RELAY4_Pin|RELAY3_Pin|RS422_TX_EN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -623,10 +621,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(NOS2_LC_DATA_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : RELAY11_Pin RELAY10_Pin RELAY2_Pin RELAY1_Pin
-                           RELAY0_Pin */
-  GPIO_InitStruct.Pin = RELAY11_Pin|RELAY10_Pin|RELAY2_Pin|RELAY1_Pin
-                          |RELAY0_Pin;
+  /*Configure GPIO pins : RELAY11_Pin RELAY10_Pin NOS2_LC_CLK_Pin RELAY2_Pin
+                           RELAY1_Pin RELAY0_Pin */
+  GPIO_InitStruct.Pin = RELAY11_Pin|RELAY10_Pin|NOS2_LC_CLK_Pin|RELAY2_Pin
+                          |RELAY1_Pin|RELAY0_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
