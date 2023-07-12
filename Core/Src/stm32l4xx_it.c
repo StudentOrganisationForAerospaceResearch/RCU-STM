cff9d8435c592405e3190da24b3d2970303f20c2
[gracejang] <grace.jang1@ucalgary.ca>
2023-07-12 02:01:39 -0600

    added NVICSystem as well as configASSERT was modified

--- Core/Src/stm32l4xx_it.c
+++ Core/Src/stm32l4xx_it.c
@@ -93,6 +93,7 @@ void HardFault_Handler(void)
   /* USER CODE END HardFault_IRQn 0 */
   while (1)
   {
+    NVIC_SystemReset();
     /* USER CODE BEGIN W1_HardFault_IRQn 0 */
     /* USER CODE END W1_HardFault_IRQn 0 */
   }
